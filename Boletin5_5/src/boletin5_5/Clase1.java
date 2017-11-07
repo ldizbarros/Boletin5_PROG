ab28bf94f49fa79ad5fb27495e594590ca7d47d2
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2017-11-07 08:57:15 +0100

    Correccion

--- Boletin5_5/src/boletin5_5/Clase1.java
+++ Boletin5_5/src/boletin5_5/Clase1.java
@@ -4,12 +4,8 @@ public class Clase1 {
     private int num1,num2,num3;
     
     public void numMaior(int num1,int num2,int num3){
-        if (num1>num2){
-            if (num1>num3){
-                System.out.println("O numero "+num1+" e o mayor dos introducidos");
-            }else{
-                System.out.println("O numero "+num3+" e o mayor dos introducidos");
-            }
+        if (num1>num2 && num1>num3){
+            System.out.println("O numero "+num1+" e o mayor dos introducidos");
         }else if (num2>num3){
             System.out.println("O numero "+num2+" e o mayor dos introducidos");
         }else{

1f696cf842b67b33668789c0c9f43ec8e6306a4b
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2017-11-08 09:47:31 +0100

    Correccion

--- Boletin5_6/src/boletin5_6/Clase1.java
+++ Boletin5_6/src/boletin5_6/Clase1.java
@@ -7,9 +7,9 @@ public class Clase1 {
     public void visualizarTipo(String pro,int venda){
         if (venda<=100){
             System.out.println("O producto "+pro+" e de tipo baixo");
-        }else if (venda>100 && venda<=500){
+        }else if (venda<=500){
             System.out.println("O producto "+pro+" e de tipo medio");
-        }else if(venda>500 && venda<=1000){
+        }else if(venda<=1000){
             System.out.println("O producto "+pro+" e de tipo alto");
         }else{
             System.out.println("O producto "+pro+" e de tipo primeira necesidade");
